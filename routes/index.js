914f534a4b8f3b7d780972e685264f31d2cdbd68
Julio <ljuliom@gmail.com>
2014-05-24 12:41:06 -0500

    listing rooms

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title: 'Express Chat' });
 });
 
 router.get('/chatroom', function(req, res){
 	res.render('chatroom', { title: 'Express Chat' });
+});
+
+router.get('/rooms', function(req, res){
+	res.render('rooms', { title: 'Express Chat' });
 });
 
 module.exports = router;
