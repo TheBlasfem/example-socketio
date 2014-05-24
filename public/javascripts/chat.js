c371b8ddb00c2421ba991f8d0351bee42e27f27d
Julio <ljuliom@gmail.com>
2014-05-24 11:17:07 -0500

    chapter 4 rooms

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,37 +1,43 @@
 var chatInfra = io.connect('/chat_infra'),
 	  chatCom = io.connect('/chat_com');
 
-chatInfra.on('name_set', function(data){
-	$('#nameform').hide();
-	$('#messages').append('<div class="systemMessage">' + 
-	'Hello '+data.name+'</div>');
+var roomName = decodeURI(
+(RegExp("room" + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]);
 
-	chatInfra.on("user_entered", function(user){
+if (roomName){
+	chatInfra.on('name_set', function(data){
+		chatInfra.emit('join_room', {'name': roomName});
+		$('#nameform').hide();
 		$('#messages').append('<div class="systemMessage">' + 
-		user.name + ' has joined the room.' + '</div>');
+		'Hello '+data.name+'</div>');
+
+		chatInfra.on("user_entered", function(user){
+			$('#messages').append('<div class="systemMessage">' + 
+			user.name + ' has joined the room.' + '</div>');
+		});
+
+		$('#send').click(function(){ 
+			var data = { 
+				message: $('#message').val(), 
+				type:'userMessage' 
+			}; 
+			chatCom.send(JSON.stringify(data)); 
+			$('#message').val(''); 
+		}); 
+
+		chatCom.on('message', function (data) { 
+			data = JSON.parse(data); 		
+			if(data.username){
+				$('#messages').append('<div class="'+data.type+
+				'"><span class="name">' + data.username + ":</span> " + data.message + '</div>');
+			}
+			else{	
+				$('#messages').append('<div class="'+data.type+'">' +data.message + '</div>');
+			}
+		}); 
+
 	});
-
-	$('#send').click(function(){ 
-		var data = { 
-			message: $('#message').val(), 
-			type:'userMessage' 
-		}; 
-		chatCom.send(JSON.stringify(data)); 
-		$('#message').val(''); 
-	}); 
-
-	chatCom.on('message', function (data) { 
-		data = JSON.parse(data); 		
-		if(data.username){
-			$('#messages').append('<div class="'+data.type+
-			'"><span class="name">' + data.username + ":</span> " + data.message + '</div>');
-		}
-		else{	
-			$('#messages').append('<div class="'+data.type+'">' +data.message + '</div>');
-		}
-	}); 
-
-});
+}
 
 $(function(){ 
 	
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/chatroom', function(req, res){
+	res.render('chatroom', { title: 'Express Chat' });
 });
 
 module.exports = router;
